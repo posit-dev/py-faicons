a1dd2a54e52df94ed41fc01ac9982d4e314bb6de
Carson <cpsievert1@gmail.com>
2022-03-03 10:43:08 -0600

    Accomodate for recent changes to htmltools; run black for consistent formatting

--- fontawesome/fontawesome.py
+++ fontawesome/fontawesome.py
@@ -1,80 +1,85 @@
 import json
 import re
 from os.path import join, dirname
-from htmltools import tags, Tag
-# TODO: make _html_escape public
-from htmltools.util import css, _html_escape
+from htmltools import tags, Tag, css
+
+# TODO: make _html_escape public?
+from htmltools._util import _html_escape
 from typing import Dict, Literal, Optional, Union
 
 __all__ = ("icon_svg", "metadata")
 
-with open(join(dirname(__file__), 'icons.json')) as f:
+with open(join(dirname(__file__), "icons.json")) as f:
     _ICONS = json.load(f)
+
 
 def metadata():
     """
     Get the metadata for all icons.
-  
+
     Returns:
       A dictionary of icon metadata.
-    
+
     Examples:
-      
+
     """
     return _ICONS
 
-def icon_svg(name: str, 
-             style: Optional[str] = None,
-             fill: Optional[str] = "currentColor",
-             fill_opacity: Optional[str] = None,
-             stroke: Optional[str] = None,
-             stroke_width: Optional[str] = None,
-             stroke_opacity: Optional[str] = None,
-             height: Optional[str] = None,
-             width: Optional[str] = None,
-             margin_left: Optional[str] = "auto",
-             margin_right: Optional[str] = "0.2em",
-             position: Optional[str] = "relative",
-             title: Optional[str] = None,
-             a11y: Literal["deco", "sem", "none"] = "deco") -> Tag:
+
+def icon_svg(
+    name: str,
+    style: Optional[str] = None,
+    fill: Optional[str] = "currentColor",
+    fill_opacity: Optional[str] = None,
+    stroke: Optional[str] = None,
+    stroke_width: Optional[str] = None,
+    stroke_opacity: Optional[str] = None,
+    height: Optional[str] = None,
+    width: Optional[str] = None,
+    margin_left: Optional[str] = "auto",
+    margin_right: Optional[str] = "0.2em",
+    position: Optional[str] = "relative",
+    title: Optional[str] = None,
+    a11y: Literal["deco", "sem", "none"] = "deco",
+) -> Tag:
     """
     Generate Font Awesome icons as SVGs
-  
+
     Parameters:
       name: The icon's name (e.g., "play", "pause", etc).
-  
+
       style: The FontAwesome style for the icon (e.g., "regular", "solid", etc).
-  
+
       fill: The icon's fill color.
-  
+
       fill_opacity: The icon's fill opacity (0.0 - 1.0).
-  
+
       stroke: The icon's stroke color.
-  
+
       stroke_width: The icon's stroke width.
-  
+
       stroke_opacity: The icon's stroke opacity (0.0 - 1.0).
-  
+
       height: The icon's height.
-  
+
       width: The icon's width.
-  
+
       margin_left: The icon's left margin.
-  
+
       margin_right: The icon's right margin.
-  
+
       position: The icon's position (e.g., "absolute", "relative", etc).
-  
-      title: A description of the icon to display on mouse hover. 
-      When `a11y == "sem"`, this description is also used as an aria-label 
+
+      title: A description of the icon to display on mouse hover.
+      When `a11y == "sem"`, this description is also used as an aria-label
       (otherwise, the icon's inherent label is used for the aria-label).
-  
+
       a11y: Either "deco" (decorative) or "sem" (semantic). Using "none" will
       result in no accessibility features for the icon.
-    
+
     Returns:
       An HTML string containing the SVG of the icon.
-  
+
     Raises:
       ValueError: If the icon name is not valid.
     """
@@ -86,10 +91,12 @@ def icon_svg(name: str,
     styles = icon["styles"]
     style = styles[0] if style is None else style
     if style not in styles:
-        raise ValueError(f"Style {style} not found for {name} icon. Posible styles are: {styles}")
+        raise ValueError(
+            f"Style {style} not found for {name} icon. Posible styles are: {styles}"
+        )
 
     svg = icon["svg"][style]
-    
+
     h = _parse_length_unit(height)
     w = _parse_length_unit(width)
     if height is None and width is None:
@@ -110,7 +117,7 @@ def icon_svg(name: str,
         svg_attrs["role"] = "img"
     elif a11y == "sem":
         title = icon["label"] if title is None else title
-        svg_attrs["aria-label"] = _html_escape(title, attr = True)
+        svg_attrs["aria-label"] = _html_escape(title, attr=True)
         svg_attrs["role"] = "img"
 
     # N.B. this returns a tag object, not a string, because I don't think it's
@@ -119,7 +126,7 @@ def icon_svg(name: str,
     # probably make nav()'s JSX component smarter)
     return tags.svg(
         None if title is None else tags.title(_html_escape(title)),
-        tags.path(d=svg['path']),
+        Tag("path", d=svg["path"]),
         **svg_attrs,
         style=css(
             fill=fill,
@@ -132,31 +139,46 @@ def icon_svg(name: str,
             margin_left=margin_left,
             margin_right=margin_right,
             position=position,
-            vertical_align='-0.125em',
-            font_size='inherit',
-            overflow='visible',
-        )
+            vertical_align="-0.125em",
+            font_size="inherit",
+            overflow="visible",
+        ),
     )
 
 
 def _parse_length_unit(x: str) -> Dict[str, Union[str, float]]:
-  if x is None:
-      return None
+    if x is None:
+        return None
 
-  if not re.search("^^[0-9]*\\.?[0-9]+[a-z]+$", x):
-      raise ValueError("Values provided to `height` and `width` must have a numerical value followed by a CSS length unit.")
+    if not re.search("^^[0-9]*\\.?[0-9]+[a-z]+$", x):
+        raise ValueError(
+            "Values provided to `height` and `width` must have a numerical value followed by a CSS length unit."
+        )
 
-  unit = re.sub("[0-9\\.]+?", "", x)
+    unit = re.sub("[0-9\\.]+?", "", x)
 
-  if unit not in _css_length_units:
-      raise ValueError(f"{unit} is not a valid CSS length unit.")
+    if unit not in _css_length_units:
+        raise ValueError(f"{unit} is not a valid CSS length unit.")
 
-  value = float(re.sub("[a-z]+$", "", x))
-  
-  return dict(value=value, unit=unit)
+    value = float(re.sub("[a-z]+$", "", x))
+
+    return dict(value=value, unit=unit)
 
 
 _css_length_units = [
-    "cm", "mm", "in", "px", "pt", "pc", "em", "ex",
-    "ch", "rem", "vw", "vh", "vmin", "vmax", "%"
+    "cm",
+    "mm",
+    "in",
+    "px",
+    "pt",
+    "pc",
+    "em",
+    "ex",
+    "ch",
+    "rem",
+    "vw",
+    "vh",
+    "vmin",
+    "vmax",
+    "%",
 ]
