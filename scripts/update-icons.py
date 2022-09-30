f8c74c3b9a73b1fa940f5afb2b90a04ddcca8bc2
Winston Chang <winston@stdout.org>
2022-09-30 13:19:19 -0500

    Minify icons.json

--- scripts/update-icons.py
+++ scripts/update-icons.py
@@ -1,10 +1,30 @@
 #! /usr/bin/env python3
 
-import os
+from pathlib import Path
 from urllib.request import urlretrieve
+import json
 
 version = "6.2.0"
+tempfile = Path(__file__).parent.parent / "faicons/icons.json.tmp"
+destfile = Path(__file__).parent.parent / "faicons/icons.json"
+
+if tempfile.exists():
+    tempfile.unlink()
+
 urlretrieve(
     f"https://raw.githubusercontent.com/FortAwesome/Font-Awesome/{version}/metadata/icons.json",
-    os.path.join(os.path.dirname(__file__), "../faicons/icons.json"),
+    tempfile,
 )
+
+# Read in the JSON, then write it out, so that the result is minified.
+with open(tempfile) as f:
+    icon_data = json.load(f)
+
+tempfile.unlink()
+
+
+if destfile.exists():
+    destfile.unlink()
+
+with open(destfile, "w") as f:
+    json.dump(icon_data, f)
